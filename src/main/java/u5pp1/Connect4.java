7a40ea0d3aea9ef2e43d808dc1ce9a10cab78c02
Rebecca Kim <rebeccakim@MacBook-Pro-7.attlocal.net>
2023-01-11 23:49:43 -0800

    hello

--- src/main/java/u5pp1/Connect4.java
+++ src/main/java/u5pp1/Connect4.java
@@ -12,7 +12,34 @@ public class Connect4 {
     public static final int EMPTY = 0;
 
    // implementation here
+public static boolean isFull(int[][] board){
+    for (int i=0; i<6; i++){
+        for (int j=0; j<7; j++){
+            if (board[i][j] == 0){
+                return false;
+            }
+            // if (board[i][j] == 0){
+            //     return false;
+            // }
+        }
+    }
+    return true;
+}
 
+public static boolean isBoardValid(int[][] board){
+    for (int i=0; i<6; i++){
+        for (int j=0; j<7; j++){
+            if (board[i][j] == 0 && (board[i][j+1] == 1 || board[i][j+1] == -1)){
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+public static int getWinner(int[][] board){
+
+}
    // It is recommended to use private helper methods
 
 }
